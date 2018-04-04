769c943cfb1b50f1cc1e6585c3636157392bf347
niyoufa <1061794187@qq.com>
2018-04-04 10:08:12 +0800

    algorithm

---
+++ __init__.py
@@ -0,0 +1,2 @@
+# @Time    : 2018/4/4 8:45
+# @Author  : Niyoufa
---
+++ eight_queen_problem.py
@@ -0,0 +1,18 @@
+# @Time    : 2018/4/4 8:46
+# @Author  : Niyoufa
+
+N = 8
+count = 0
+chessboard = [[0]*N]*N
+
+def print_chessboard():
+    for row in chessboard:
+        print(row)
+
+def check(i, j):
+    print(i, j)
+
+
+if __name__ == "__main__":
+    print_chessboard()
+
